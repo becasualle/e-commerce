c9594ba5e2c588fffab38fe456a9db2c7f85203e
Mark <mark.nelubin@gmail.com>
2021-08-24 18:35:59 +0300

    Set up React Router

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from './AboutPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Cart from './CartPage'
+import Error from './ErrorPage'
+import Home from './HomePage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+
+export {
+    About,
+    Products,
+    SingleProduct,
+    Cart,
+    Error,
+    Home,
+    Checkout,
+    PrivateRoute
+}
